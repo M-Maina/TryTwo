0f72ca56fc71ad8efc92445f115897decab5e284
wamah <pythondeveloper21@gmail.com>
2021-07-19 23:14:08 +0100

    simple updates

--- trytwo/settings.py
+++ trytwo/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #Third party apps
     'prof',
     'contact',
     'crispy_forms',
